4c66819d41b63bb3e345e4681e43e88aafe76763
Adam Gleichsner <amg188@case.edu>
2016-09-16 12:29:05 -0400

    Prep for spark

--- app/controllers/api/v1/letter_controller.rb
+++ app/controllers/api/v1/letter_controller.rb
@@ -7,13 +7,14 @@ class Api::V1::LetterController < ApplicationController
     session = get_session
     subject = params[:subject]
     content = params[:content]
+    mailbox_id = params[:mailbox_id]
     to_user = User.find(params[:to_user_id])
     from_user = User.find(session[:id])
 
     sf = SentFromUser.create(user: from_user)
     st = SentToUser.create(user: to_user)
 
-    render json: Letter.create(subject: subject, content: content, sent_from_user:sf, sent_to_user:st)
+    render json: Letter.create(subject: subject, content: content, sent_from_user:sf, sent_to_user:st, mailbox_id: mail)
   end
 
 end
